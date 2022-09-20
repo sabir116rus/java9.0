84bcba8a68270f648fc2c06bdadff1a980990f0f
Альберт <sabir116rus@gmail.com>
2022-09-20 12:08:55 +0300

    curly braces and early exit fixes

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -8,18 +8,23 @@ public class Radio {
 
     public void setStation(int station) {
         this.station = station;
+        return;
     }
 
     public void next() {
         if (station < 9) {
             station = station + 1;
-        } else station = 0;
+        } else {
+            station = 0;
+        }
     }
 
     public void prev() {
         if (station > 0) {
             station = station - 1;
-        } else station = 9;
+        } else {
+            station = 9;
+        }
     }
 
     public int getVolume() {
@@ -28,6 +33,7 @@ public class Radio {
 
     public void setVolume(int volume) {
         this.volume = volume;
+        return;
     }
 
 
